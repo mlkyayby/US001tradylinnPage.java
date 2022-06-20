a963fecfeeb6fca29b7b63b48b27cd23519bc58c
EceNarin <la.crymosa321@gmail.com>
2022-06-20 23:36:01 +0300

    ecenarin

--- src/test/java/Pages/TradyLinnPages.java
+++ src/test/java/Pages/TradyLinnPages.java
@@ -5,10 +5,69 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class TradyLinnPages {
     public TradyLinnPages(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
-    @FindBy
-    public WebElement x;
+    @FindBy(xpath = "//*[@id=\"header\"]/div/div[1]/div/div[2]/div/div[3]/div/div/a[1]/span")
+    public WebElement girisyap;
+    @FindBy(xpath = "(//input[@id=\"username\"])[1]")
+    public WebElement email;
+    @FindBy(xpath = "(//input[@id=\"password\"])[1]")
+    public WebElement password;
+    @FindBy(xpath = "(//button[@value=\"Giriş Yap\"])[1]")
+    public WebElement login;
+    @FindBy(xpath = "//*[@id=\"menu-item-1074\"]/a")
+    public WebElement hesabim;
+    @FindBy(linkText = "Store Manager")
+    public WebElement storeManager;
+    @FindBy(xpath = "//a[@data-tip=\"Add New Product\"]")
+    public WebElement addNewProduct;
+    @FindBy(xpath = "//input[@id=\"is_virtual\"]")
+    public WebElement virtualCheckBox;
+    @FindBy(xpath = "//input[@id=\"is_downloadable\"]")
+    public WebElement dowloadablecheckBox ;
+    @FindBy(xpath = "//input[@id=\"pro_title\"]")
+    public WebElement productName;
+    @FindBy(xpath = "//img[@id=\"featured_img_display\"]")
+    public WebElement photo;
+    @FindBy(xpath = "//*[@id=\"wcfm_menu\"]/div")
+    public List<WebElement> myStoreMenu;
+    @FindBy(xpath = "//*[@id=\"wcfm-products\"]/thead/tr/th")
+    public List<WebElement> tableList;
+    @FindBy(xpath = "//*[@id=\"wcfm-products\"]/tbody/tr/td")
+    public WebElement emptyBody;
+    @FindBy(id = "__wp-uploader-id-1")
+    public WebElement uploadFile;
+    @FindBy(xpath = "//*[@id=\"__wp-uploader-id-0\"]/div[4]/div/div[2]/button")
+    public WebElement sec;
+    @FindBy(xpath = "//iframe[@id=\"excerpt_ifr\"]")
+    public WebElement iframe;
+    @FindBy(xpath = "//*[@id=\"tinymce\"]")
+    public WebElement description;
+    public WebElement choiceElement(List<WebElement> myStoreMenu,String str){
+        WebElement element=null;
+        for (WebElement w:myStoreMenu) {
+            if(w.getText().equalsIgnoreCase(str)){
+                   element=w;
+            }
+        }
+        return element;
+    }
+    public WebElement choiceContainsElement(List<WebElement> myStoreMenu,String str){
+        WebElement element=null;
+        for (WebElement w:myStoreMenu) {
+            if(w.getText().toLowerCase().contains(str.toLowerCase())){
+                element=w;
+            }
+        }
+        return element;
+    }
+
+
+
+
+
 }
--- src/test/java/Tests/US_005.java
+++ src/test/java/Tests/US_005.java
@@ -1,21 +1,109 @@
 package Tests;
 
+import Pages.TradyLinnPages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class US_005 {
+    TradyLinnPages trd=new TradyLinnPages();
+    Actions action=new Actions(Driver.getDriver());
+    SoftAssert soft=new SoftAssert();
     @Test
-    public void TC01(){
-
+    public void TC01DisableProductList() throws InterruptedException {
+        // User goes to tradylinn.com
+          Driver.getDriver().get(ConfigReader.getProperty("tradyUrl"));
+        //        User clicks "Giriş Yap"
+         trd.girisyap.click();
+        //        User enters email
+        trd.email.sendKeys(ConfigReader.getProperty("userMail"));
+        //        User enters password
+        trd.password.sendKeys(ConfigReader.getProperty("userPassword"));
+        //        User clicks "Giriş Yap" button
+        trd.login.click();
+        //        User clicks "Hesabım"
+        //ReusableMethods.waitForClickablility(trd.hesabim,10);
+        Thread.sleep(5000);
+        trd.hesabim.click();
+        //        User clicks "Store Manager"
+        trd.storeManager.click();
+        //        User clicks "Ürün"
+        trd.choiceContainsElement(trd.myStoreMenu,"Ürün").click();
+        //        User scrolls down the page
+        action.sendKeys(Keys.DOWN).perform();
+        //        User verifies status, stock, price, date column head if displayed
+        List<String> arr= Arrays.asList("Status","Stock","Price","Date");
+        for(int i=0;i< arr.size();i++){
+          soft.assertTrue(trd.choiceElement(trd.tableList, arr.get(i)).isDisplayed());
+        }
     }
-    @Test
-    public void TC02(){
-
+    @Test (priority = 2)
+    public void TC02AddNew(){
+        //  User clicks "Yeni ekle" button
+        trd.addNewProduct.click();
+        //  User verifies "Virtual" checkbox is displayed
+        soft.assertTrue(trd.virtualCheckBox.isDisplayed());
+        // User click "virtual" checkbox
+        trd.virtualCheckBox.click();
+        //   User verifies "Downloadable" checkbox is displayed
+        soft.assertTrue(trd.dowloadablecheckBox.isDisplayed());
+        // User click "virtual" checkbox
+        trd.dowloadablecheckBox.click();
     }
-    @Test
-    public void TC03(){
+    @Test (priority = 3)
+    public void TC03UrunEkle(){
+        //User enters "Product Name"
+        trd.productName.sendKeys("Bursa'daki Kaynana cinayetlerinin sirlari");
+        //User enters "Sale Price"
+        action.click(trd.productName).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys("70").perform();
+    }  @Test(priority = 4)
+    public void TC04() throws InterruptedException, AWTException {
+        // User clicks photo icon
+           trd.photo.click();
+        // User clicks "Dosya Seçin" button
+        JavascriptExecutor js = (JavascriptExecutor)Driver.getDriver();
+        js.executeScript("window.scrollBy(0,200)");
+        trd.uploadFile.click();
+        Thread.sleep(2000);
+        Robot rb = new Robot();
+        StringSelection str = new StringSelection("C:\\Users\\himer\\OneDrive\\Masaüstü\\kaynana.jpg");
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(str, null);
+        rb.keyPress(KeyEvent.VK_CONTROL);
+        rb.keyPress(KeyEvent.VK_V);
+        rb.keyRelease(KeyEvent.VK_CONTROL);
+        rb.keyRelease(KeyEvent.VK_V);
+        rb.keyPress(KeyEvent.VK_ENTER);
+        rb.keyRelease(KeyEvent.VK_ENTER);
+        // User selects product photo and click open button
 
-    }  @Test
-    public void TC04(){
+        // User waits for uploading file
+        Thread.sleep(5000);
+        // User clicks "ADD TO GALLERY" button
+        trd.sec.click();
+    }
+    @Test(priority = 5)
+    public void TC05Description(){
+        //Driver.getDriver().switchTo().frame(trd.iframe);
+        action.sendKeys(Keys.DOWN).sendKeys(Keys.DOWN).perform();
+      Driver.getDriver().findElement(By.xpath("//*[@id=\"excerpt\"]")).sendKeys("kvjfjfjjgjgjgjgjhjhjhjhjhjjn");
+    }
+    @Test (priority = 6)
+    public void US_006_TC01 (){
 
     }
 
